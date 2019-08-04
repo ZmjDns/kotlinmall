cdc347e36a701a01248185bddede59c63a5bae3d
ZmjDns <292570927@qq.com>
2019-08-04 21:09:25 +0800

    Retrofit基础配置

---
+++ baselaribary/src/main/java/com/zmj/base/common/BaseConstant.kt
@@ -0,0 +1,15 @@
+package com.zmj.base.common
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/8/1
+ * Description :
+ */
+class BaseConstant {
+
+    companion object{
+        val Server_address = "http:192.168.1.101/"
+    }
+}
