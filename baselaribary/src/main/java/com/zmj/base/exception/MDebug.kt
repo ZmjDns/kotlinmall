e30d736894eefd8abf85fda25277b6aaca9c933a
ZmjDns <292570927@qq.com>
2019-08-07 22:44:40 +0800

    收集bug信息

---
+++ baselaribary/src/main/java/com/zmj/base/exception/MDebug.kt
@@ -0,0 +1,11 @@
+package com.zmj.base.exception
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/8/7
+ * Description : bug收集
+ */
+class MDebug {
+}
