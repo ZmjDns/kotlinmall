73889d108a5d5d3e6cb4ae24643e603924927e9c
ZmjDns <292570927@qq.com>
2019-08-08 17:42:02 +0800

    捕捉异常日志类

--- baselaribary/src/main/java/com/zmj/base/exception/MDebug.kt
+++ baselaribary/src/main/java/com/zmj/base/exception/MDebug.kt
@@ -1,5 +1,9 @@
 package com.zmj.base.exception
 
+import com.zmj.base.ext.getNowTime
+import java.io.File
+import java.lang.Exception
+
 /**
  * Author : Zmj
  * Blog : https://blog.csdn.net/Zmj_Dns
@@ -8,4 +12,53 @@ package com.zmj.base.exception
  * Description : bug收集
  */
 class MDebug {
+    private var debugFile: File? = null
+
+    private constructor(fileName: String){
+        debugFile = File(fileName)
+    }
+
+    companion object{
+        @Volatile
+        var instance: MDebug? = null
+
+        fun getInstance(fileName: String): MDebug{
+            if (instance == null){
+                synchronized(MDebug::class){
+                    if (instance == null){
+                        MDebug(fileName)
+                    }
+                }
+            }
+            return instance!!
+        }
+    }
+
+    fun writeInfo(tag: String,info: String){
+        writeStringToFile("$tag:  $info")
+    }
+
+    fun writeThrowable(tag: String,throwable: Throwable){
+        writeStringToFile("$tag:  ${throwable.message}")
+    }
+
+    fun writeException(tag: String,exception: Exception){
+        writeStringToFile("$tag:  ${exception.message}")
+    }
+
+    @Synchronized private fun writeStringToFile(content: String){
+        val str = "${getNowTime()}:  $content"
+        if (isFileOK()){
+            debugFile!!.appendText(str)
+        }
+    }
+
+    private fun isFileOK(): Boolean{
+        if (debugFile != null){
+            if (debugFile!!.exists()){
+                return debugFile!!.canWrite()
+            }
+        }
+        return false
+    }
 }
