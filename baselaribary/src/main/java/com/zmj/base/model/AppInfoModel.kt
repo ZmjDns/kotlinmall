608f9dd1224d9c39b022f9b2edff0fdd964288cd
ZmjDns <292570927@qq.com>
2019-08-10 22:47:43 +0800

    获取app版本model

---
+++ baselaribary/src/main/java/com/zmj/base/model/AppInfoModel.kt
@@ -0,0 +1,19 @@
+package com.zmj.base.model
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/8/10
+ * Description :app版本信息，更新功能
+ */
+class AppInfoModel {
+    fun getAppVersion(){
+
+    }
+
+
+    fun downloadNewVersionApk(){
+
+    }
+}
