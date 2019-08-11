4849fd4486295ca472814fd4d96a86c28c9c3228
ZmjDns <292570927@qq.com>
2019-08-11 22:36:19 +0800

    获取app信息Persenter

---
+++ baselaribary/src/main/java/com/zmj/base/presenter/AppInfoPersenter.kt
@@ -0,0 +1,13 @@
+package com.zmj.base.presenter
+
+import com.zmj.base.presenter.view.BaseView
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/8/11
+ * Description : 获取app信息Persenter
+ */
+class AppInfoPersenter:BasePresenter<BaseView>() {
+}
