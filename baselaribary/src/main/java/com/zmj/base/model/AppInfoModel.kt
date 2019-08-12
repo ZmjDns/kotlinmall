0eaa9af05ea568fae8401dae0ee3ca315d252241
ZmjDns <292570927@qq.com>
2019-08-12 15:05:04 +0800

    获取app信息model+persenter

--- baselaribary/src/main/java/com/zmj/base/model/AppInfoModel.kt
+++ baselaribary/src/main/java/com/zmj/base/model/AppInfoModel.kt
@@ -1,5 +1,13 @@
 package com.zmj.base.model
 
+import com.zmj.base.data.entry.AppInfo
+import com.zmj.base.data.net.ApiService
+import com.zmj.base.data.net.IResponse
+import com.zmj.base.data.net.RetrofitFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+
 /**
  * Author : Zmj
  * Blog : https://blog.csdn.net/Zmj_Dns
@@ -8,12 +16,29 @@ package com.zmj.base.model
  * Description :app版本信息，更新功能
  */
 class AppInfoModel {
-    fun getAppVersion(){
-
+    //获取版本信息
+    fun getAppVersion(listener: IResponse<AppInfo>){
+        MainScope().launch(Dispatchers.IO) {
+            kotlin.runCatching {
+                RetrofitFactory.instance.create(ApiService::class.java).getAppVersionInfo()
+            }.onSuccess {
+                listener.onSuccess(it.data)
+            }.onFailure {
+                listener.onFailed(it)
+            }
+        }
     }
 
-
+    //下载Apk
     fun downloadNewVersionApk(){
+        MainScope().launch(Dispatchers.IO) {
+            kotlin.runCatching {
 
+            }.onSuccess {
+
+            }.onFailure {
+
+            }
+        }
     }
 }
--- baselaribary/src/main/java/com/zmj/base/presenter/AppInfoPersenter.kt
+++ baselaribary/src/main/java/com/zmj/base/presenter/AppInfoPersenter.kt
@@ -1,5 +1,9 @@
 package com.zmj.base.presenter
 
+import com.zmj.base.data.entry.AppInfo
+import com.zmj.base.data.net.IResponse
+import com.zmj.base.model.AppInfoModel
+import com.zmj.base.presenter.view.AppVersionView
 import com.zmj.base.presenter.view.BaseView
 
 /**
@@ -9,9 +13,20 @@ import com.zmj.base.presenter.view.BaseView
  * Time : 2019/8/11
  * Description : 获取app信息Persenter
  */
-class AppInfoPersenter:BasePresenter<BaseView>() {
-    fun getAppInfo(){
+class AppInfoPersenter:BasePresenter<AppVersionView>() {
 
+    private val appInfoModel: AppInfoModel by lazy { AppInfoModel() }
+
+    fun getAppInfo(){
+        mView.showLoading()
+        appInfoModel.getAppVersion(object :IResponse<AppInfo>{
+            override fun onSuccess(response: AppInfo) {
+                mView.onGetVersion(response)
+            }
+            override fun onFailed(throwable: Throwable) {
+                mView.onError(throwable)
+            }
+        })
     }
 
     fun downloadNewAppVersion(){
