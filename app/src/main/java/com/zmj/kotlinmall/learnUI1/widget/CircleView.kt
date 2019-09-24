cb7364b3bedad204dc63cb12d1744892a1eceea2
ZmjDns <292570927@qq.com>
2019-09-24 22:52:39 +0800

    自定义画的View

--- app/src/main/java/com/zmj/kotlinmall/learnUI1/widget/CircleView.kt
+++ app/src/main/java/com/zmj/kotlinmall/learnUI1/widget/CircleView.kt
@@ -1,10 +1,7 @@
 package com.zmj.kotlinmall.learnUI1.widget
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import org.jetbrains.annotations.Nullable
@@ -52,4 +49,84 @@ class CircleView: View{
     }
 }
 
-class RectangleView
+class RectangleView: View{
+    private val paint: Paint
+
+    constructor(context: Context): super(context)
+    constructor(context: Context,@Nullable attrs: AttributeSet): super(context,attrs)
+    constructor(context: Context,@Nullable attrs: AttributeSet,@Nullable defStyleAttr: Int): super(context,attrs,defStyleAttr)
+
+    init {
+        paint = Paint()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        //画矩形
+        //paint.style = Paint.Style.FILL
+        paint.style = Paint.Style.STROKE
+        paint.color = Color.BLUE
+        paint.strokeWidth = 2f
+        canvas?.drawRect(0f,0f,200f,200f,paint)
+
+
+        paint.style = Paint.Style.FILL_AND_STROKE
+        val rect = Rect(250,0,450,200)
+        canvas?.drawRect(rect,paint)
+    }
+}
+
+class PointView: View{
+    constructor(context: Context): super(context)
+    constructor(context: Context,@Nullable attrs: AttributeSet): super(context,attrs)
+    constructor(context: Context,@Nullable attrs: AttributeSet,@Nullable defStyleAttr: Int): super(context,attrs, defStyleAttr)
+
+    private val paint: Paint
+    init {
+        paint = Paint()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        paint.color = Color.RED
+        //画圆
+        paint.strokeWidth = 20f
+        paint.strokeCap = Paint.Cap.BUTT
+        canvas?.drawPoint(50f,30f,paint)
+
+        paint.strokeCap = Paint.Cap.ROUND
+        canvas?.drawPoint(100f,30f,paint)
+
+        paint.strokeCap = Paint.Cap.SQUARE
+        canvas?.drawPoint(150f,30f,paint)
+
+        //批量绘点
+        val points = floatArrayOf(20f,70f,70f,70f,120f,70f,170f,70f,220f,70f,290f,70f)
+        canvas?.drawPoints(points,paint)
+
+    }
+}
+
+class OvalView: View{
+    constructor(context: Context): super(context)
+    constructor(context: Context,@Nullable attrs: AttributeSet): super(context)
+    constructor(context: Context,@Nullable attrs: AttributeSet,@Nullable defStyleAttr: Int): super(context,attrs,defStyleAttr)
+
+    private val paint = Paint()
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        paint.color = Color.CYAN
+        paint.strokeWidth = 2f
+        paint.style = Paint.Style.STROKE
+        //画椭圆
+        canvas?.drawOval(0f,0f,200f,160f,paint)
+
+        paint.style = Paint.Style.FILL_AND_STROKE
+        canvas?.drawOval(220f,0f,420f,160f,paint)
+
+    }
+}
