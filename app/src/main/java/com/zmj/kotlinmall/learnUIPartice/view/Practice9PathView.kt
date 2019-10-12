9e014cb2e59bf42ceb718858df824fed22de15f8
ZmjDns <292570927@qq.com>
2019-10-12 20:43:40 +0800

    drawPath

---
+++ app/src/main/java/com/zmj/kotlinmall/learnUIPartice/view/Practice9PathView.kt
@@ -0,0 +1,47 @@
+package com.zmj.kotlinmall.learnUIPartice.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+import org.jetbrains.annotations.Nullable
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/10/11
+ * Description :
+ */
+class Practice9PathView: View {
+
+    constructor(context: Context): super(context)
+    constructor(context: Context, @Nullable attrs: AttributeSet): super(context, attrs)
+    constructor(context: Context, @Nullable attrs: AttributeSet, @Nullable defStyle: Int): super(context, attrs,defStyle)
+
+    val paint = Paint()
+    val path = Path()
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        paint.color = Color.BLACK
+
+        paint.style = Paint.Style.FILL
+        paint.strokeWidth = 2f
+
+        path.moveTo(50f,100f)
+        path.addArc(0f,0f,100f,100f,-180f,180f)
+        path.addArc(100f,0f,200f,100f,-180f,180f)
+        path.lineTo(100f,200f)
+        path.lineTo(0f,50f)
+        //path.close()
+
+        canvas?.drawPath(path,paint)
+
+
+    }
+}
