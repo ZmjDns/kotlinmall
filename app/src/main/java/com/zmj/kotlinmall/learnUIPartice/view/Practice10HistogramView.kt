8beef0cb1b99fc78e1f859da4da03ff2c123199a
ZmjDns <292570927@qq.com>
2019-10-15 17:32:53 +0800

    zz

--- app/src/main/java/com/zmj/kotlinmall/learnUIPartice/view/Practice10HistogramView.kt
+++ app/src/main/java/com/zmj/kotlinmall/learnUIPartice/view/Practice10HistogramView.kt
@@ -1,5 +1,14 @@
 package com.zmj.kotlinmall.learnUIPartice.view
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+import org.jetbrains.annotations.Nullable
+
 /**
  * Author : Zmj
  * Blog : https://blog.csdn.net/Zmj_Dns
@@ -7,5 +16,21 @@ package com.zmj.kotlinmall.learnUIPartice.view
  * Time : 2019/10/14
  * Description :
  */
-class Practice10HistogramView {
+class Practice10HistogramView : View{
+
+    constructor(context: Context): super(context)
+    constructor(context: Context, @Nullable attrs: AttributeSet): super(context, attrs)
+    constructor(context: Context, @Nullable attrs: AttributeSet, @Nullable defStyle: Int): super(context, attrs,defStyle)
+
+    val paint = Paint()
+    val path = Path()
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        paint.color = Color.GREEN
+
+
+    }
+
 }
