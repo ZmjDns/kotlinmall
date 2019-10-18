cfb08fbe73002d726a7db822bfc4c122b9d01d9c
ZmjDns <292570927@qq.com>
2019-10-18 18:04:05 +0800

    pieView

--- app/src/main/java/com/zmj/kotlinmall/learnUIPartice/view/Practice11PieChart.kt
+++ app/src/main/java/com/zmj/kotlinmall/learnUIPartice/view/Practice11PieChart.kt
@@ -2,6 +2,7 @@ package com.zmj.kotlinmall.learnUIPartice.view
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
@@ -27,7 +28,16 @@ class Practice11PieChart: View {
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        paint.style = Paint.Style.FILL
+        paint.color = Color.RED
 
+        canvas?.drawArc(100f,100f,600f,600f,-180f,100f,true,paint)
+
+        paint.color = Color.RED
+        paint.strokeWidth = 2f
+        path.moveTo(80f,150f)
+        path.rLineTo(100f,0f)
+        path.lineTo(173.3f,273.3f)
 
     }
 
