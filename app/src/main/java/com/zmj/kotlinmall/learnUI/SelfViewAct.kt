09105f50110d162747d698af6bc31b8795308b0e
ZmjDns <292570927@qq.com>
2019-11-01 18:42:20 +0800

    展示界面

---
+++ app/src/main/java/com/zmj/kotlinmall/learnUI/SelfViewAct.kt
@@ -0,0 +1,32 @@
+package com.zmj.kotlinmall.learnUI
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.zmj.kotlinmall.R
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/9/23
+ * Description :
+ */
+class SelfViewAct: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //setContentView(R.layout.self_view_act)
+        setContentView(R.layout.self_vie_plugin)
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
