928047fe82496334e7839fb6f229b2c547b620f7
ZmjDns <292570927@qq.com>
2019-11-10 19:46:44 +0800

    。。。

--- app/src/main/java/com/zmj/kotlinmall/App.kt
+++ app/src/main/java/com/zmj/kotlinmall/App.kt
@@ -3,11 +3,7 @@ package com.zmj.kotlinmall
 import android.support.multidex.MultiDexApplication
 
 /**
- * Author : Zmj
- * Blog : https://blog.csdn.net/Zmj_Dns
- * DitHub : https://github.com/ZmjDns
- * Time : 2019/11/7
- * Description :
+ *
  */
 class App:MultiDexApplication() {
 
