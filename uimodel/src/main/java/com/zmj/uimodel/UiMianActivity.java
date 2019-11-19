3937c562082cde083aad3e21a3d292a2c0dd6ab1
ZmjDns <292570927@qq.com>
2019-11-19 22:45:21 +0800

    uiModel模块配置

---
+++ uimodel/src/main/java/com/zmj/uimodel/UiMianActivity.java
@@ -0,0 +1,13 @@
+package com.zmj.uimodel;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/11/17
+ * Description :
+ */
+public class UiMianActivity extends AppCompatActivity {
+}
