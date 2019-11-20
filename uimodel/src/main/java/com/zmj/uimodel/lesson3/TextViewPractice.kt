e88f3614241a1815ea091140fc190bc4510e2c7c
ZmjDns <292570927@qq.com>
2019-11-20 22:09:14 +0800

    TextView练习

---
+++ uimodel/src/main/java/com/zmj/uimodel/lesson3/TextViewPractice.kt
@@ -0,0 +1,11 @@
+package com.zmj.uimodel.lesson3
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/11/20
+ * Description :
+ */
+class TextViewPractice {
+}
