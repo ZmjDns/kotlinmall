7d90e7bd82c2b2e6a33b76e4474898b28afe7663
ZmjDns <292570927@qq.com>
2019-11-25 21:55:04 +0800

    TestUI

--- uimodel/src/main/java/com/zmj/uimodel/lesson3/TextViewPractice.kt
+++ uimodel/src/main/java/com/zmj/uimodel/lesson3/TextViewPractice.kt
@@ -1,13 +1,10 @@
 package com.zmj.uimodel.lesson3
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.CornerPathEffect
-import android.graphics.Paint
-import android.graphics.Path
-import android.media.effect.Effect
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import com.zmj.uimodel.R
 import org.jetbrains.annotations.Nullable
 
 /**
@@ -18,6 +15,7 @@ import org.jetbrains.annotations.Nullable
  * Description :
  */
 class TextViewPractice: View {
+
     constructor(context: Context): super(context)
     constructor(context: Context,@Nullable attrs: AttributeSet): super(context, attrs)
     constructor(context: Context,@Nullable attrs: AttributeSet,@Nullable defStyle: Int): super(context, attrs,defStyle)
@@ -28,13 +26,48 @@ class TextViewPractice: View {
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        paint.pathEffect = effect
+        paint.style = Paint.Style.STROKE
+        paint.color = Color.RED
+        paint.strokeWidth = 2f
+        //paint.pathEffect = effect
 
-        path.rLineTo(150f,300f)
+        path.lineTo(150f,300f)
         path.rLineTo(150f,-150f)
         canvas?.drawPath(path,paint);
 
-        canvas?.drawTextOnPath("HELLO ZMJ",path,0f,0f,paint)
+        //canvas?.drawTextOnPath("HELLO ZMJ",path,0f,0f,paint)
+    }
+
+}
+
+class TestText: View{
+    constructor(context: Context): super(context)
+    constructor(context: Context, @Nullable attrs: AttributeSet): super(context, attrs)
+    constructor(context: Context, @Nullable attrs: AttributeSet, @Nullable defStyle: Int): super(context, attrs,defStyle)
+
+
+    /*val paint = Paint()
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+//        paint.style = Paint.Style.STROKE
+//        paint.strokeWidth = 2f
+//        paint.color = Color.RED
+
+
+        canvas?.drawText("HELLO ZMJ .",110f,110f,paint)
+
+    }*/
+
+    val paint = Paint()
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        //paint.setMaskFilter(EmbossMaskFilter())
+
+        canvas?.drawText("HAHHAHAH",20f,100f,paint)
     }
 
 }
