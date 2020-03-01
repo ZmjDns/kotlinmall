3f2d53a473820b5d0c70cddbecfc49f42cdf8b7c
ZmjDns <292570927@qq.com>
2020-03-01 22:35:47 +0800

    baseApp

--- baselaribary/src/main/java/com/zmj/base/MyBaseApp.kt
+++ baselaribary/src/main/java/com/zmj/base/MyBaseApp.kt
@@ -1,6 +1,7 @@
 package com.zmj.base
 
 import android.app.Application
+import android.content.Context
 import com.zmj.base.exception.MDebug
 import com.zmj.base.ext.getMenuFactory
 import com.zmj.base.ext.getTypeNumber
@@ -14,15 +15,29 @@ import com.zmj.base.ext.getTypeNumber
  */
 class MyBaseApp: Application() {
 
+    private val application: MyBaseApp = this
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+
+        //分包必须最先初始化
+
+
+    }
+
     override fun onCreate() {
         super.onCreate()
-
-
-
 
 
         //开启捕捉日志
         val debufilePaths =  cacheDir.absolutePath + "/" + getMenuFactory() + "_" + getTypeNumber() + ".txt"
         MDebug.getInstance(debufilePaths)
+    }
+
+    /**
+     * 获取全局唯一的上下文
+     */
+    fun getBaseApplication():MyBaseApp{
+        return application
     }
 }
