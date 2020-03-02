e0113beb3efd0fc18102e461f2c636fd61c384c4
ZmjDns <292570927@qq.com>
2020-03-02 22:31:06 +0800

    动态注册广播

--- app/src/main/java/com/zmj/kotlinmall/broadcast/SendBroadCastActivity.kt
+++ app/src/main/java/com/zmj/kotlinmall/broadcast/SendBroadCastActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.support.v4.content.LocalBroadcastManager
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import com.zmj.kotlinmall.Manifest
 import com.zmj.kotlinmall.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -44,10 +43,10 @@ class SendBroadCastActivity: AppCompatActivity() {
         //动态注册
         val  intentFilter = IntentFilter("com.zmj.kotlinmall.MY_ACTION")
         netChangeReceiver = NetChangeReceiver()
-        registerReceiver(netChangeReceiver,intentFilter,Manifest.permission.SEND_NET_INFO,null)
+        registerReceiver(netChangeReceiver,intentFilter, "com.zmj.kotlinmall.SEND_NET_INFO",null)
         Log.i("sendMyBroadCast","广播已经注册完成")
         val intent = Intent("com.zmj.kotlinmall.MY_ACTION")
-        sendBroadcast(intent,Manifest.permission.RECEIVE_NET_INFO)
+        sendBroadcast(intent,"com.zmj.kotlinmall.RECEIVE_NET_INFO")
         //sendOrderedBroadcast(intentFilter,Manifest.permission.SEND_NET_INFO)
         Log.i("sendMyBroadCast","广播已经发出去......")
     }
